83e484cb21ab8d5a9227c6b25f1c8f1b651ac172
Dhanarooban1 <dhanazx1@gmail.com>
2024-12-19 00:15:43 +0530

    added

--- Content.js
+++ Content.js
@@ -1,3 +1,4 @@
+
 window.onload = function () {
   let videoElement = document.querySelector('video');
   if (!videoElement) return; // Ensure video element exists
@@ -5,12 +6,19 @@ window.onload = function () {
   videoElement.addEventListener('timeupdate', () => {
       let progress = (videoElement.currentTime / videoElement.duration) * 100;
       let videoTitle = document.title;
-      chrome.storage.local.set({ videoProgress: progress.toFixed(2),title: videoTitle}, () => {
+
+      chrome.runtime.sendMessage({
+        type: "progress_update",
+        videoProgress: progress.toFixed(2),
+        videoTitle: videoTitle
+    });
+    
+      chrome.storage.sync.set({ videoProgress: progress.toFixed(2),title: videoTitle}, () => {
           console.log('Progress stored:', progress.toFixed(2));
       });
 
       if (progress === 100) {
-          chrome.storage.local.set({ videoCompleted: true }, () => {
+          chrome.storage.sync.set({ videoCompleted: true }, () => {
              
           });
       }
